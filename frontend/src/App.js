c91ec6e3bdc57137d90eaa192c29dab21a258363
ray-chunkit-chung <ray.chunkit.chung@gmail.com>
2023-09-27 11:36:32 +0000

    change pic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1243,59 +1243,61 @@ function App() {
 
                                         <div class="showcase">
 
-                                        <div class="showcase-banner">
+                                            <div class="showcase-banner">
 
-                                            <img src="assets/images/products/jacket-3.jpg" alt="Mens Winter Leathers Jackets" width="300" class="product-img default" />
-                                            <img src="assets/images/products/jacket-4.jpg" alt="Mens Winter Leathers Jackets" width="300" class="product-img hover" />
+                                                <img src="assets/images/products/shirt-1.jpg" alt="Pure Garment Dyed Cotton Shirt" class="product-img default"
+                                                    width="300" />
+                                                <img src="assets/images/products/shirt-2.jpg" alt="Pure Garment Dyed Cotton Shirt" class="product-img hover"
+                                                    width="300" />
 
-                                            <p class="showcase-badge">{product.completed}</p>
+                                                <p class="showcase-badge">{product.completed}</p>
 
-                                            <div class="showcase-actions">
+                                                <div class="showcase-actions">
 
-                                                <button class="btn-action">
-                                                    <ion-icon name="heart-outline"></ion-icon>
-                                                </button>
+                                                    <button class="btn-action">
+                                                        <ion-icon name="heart-outline"></ion-icon>
+                                                    </button>
 
-                                                <button class="btn-action">
-                                                    <ion-icon name="eye-outline"></ion-icon>
-                                                </button>
+                                                    <button class="btn-action">
+                                                        <ion-icon name="eye-outline"></ion-icon>
+                                                    </button>
 
-                                                <button class="btn-action">
-                                                    <ion-icon name="repeat-outline"></ion-icon>
-                                                </button>
+                                                    <button class="btn-action">
+                                                        <ion-icon name="repeat-outline"></ion-icon>
+                                                    </button>
 
-                                                <button class="btn-action">
-                                                    <ion-icon name="bag-add-outline"></ion-icon>
-                                                </button>
+                                                    <button class="btn-action">
+                                                        <ion-icon name="bag-add-outline"></ion-icon>
+                                                    </button>
+
+                                                </div>
+
+                                            </div>
+
+                                            <div class="showcase-content">
+
+                                                <a href="#" class="showcase-category">User: {product.userId}/Product: {product.id}</a>
+
+                                                <a href="#">
+                                                    <h3 class="showcase-title">{product.title}</h3>
+                                                </a>
+
+                                                <div class="showcase-rating">
+                                                    <ion-icon name="star"></ion-icon>
+                                                    <ion-icon name="star"></ion-icon>
+                                                    <ion-icon name="star"></ion-icon>
+                                                    <ion-icon name="star-outline"></ion-icon>
+                                                    <ion-icon name="star-outline"></ion-icon>
+                                                </div>
+
+                                                <div class="price-box">
+                                                    <p class="price">$48.00</p>
+                                                    <del>$75.00</del>
+                                                </div>
 
                                             </div>
 
                                         </div>
-
-                                        <div class="showcase-content">
-
-                                            <a href="#" class="showcase-category">User: {product.userId}/Product: {product.id}</a>
-
-                                            <a href="#">
-                                                <h3 class="showcase-title">{product.title}</h3>
-                                            </a>
-
-                                            <div class="showcase-rating">
-                                                <ion-icon name="star"></ion-icon>
-                                                <ion-icon name="star"></ion-icon>
-                                                <ion-icon name="star"></ion-icon>
-                                                <ion-icon name="star-outline"></ion-icon>
-                                                <ion-icon name="star-outline"></ion-icon>
-                                            </div>
-
-                                            <div class="price-box">
-                                                <p class="price">$48.00</p>
-                                                <del>$75.00</del>
-                                            </div>
-
-                                        </div>
-
-                                    </div>
 
                                     ))}
 
